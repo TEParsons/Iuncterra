e1dfc181069f75ccd417f5e65f76f31ad64d7c4c
Todd Parsons <todd.e.parsons@googlemail.com>
2022-12-04 09:59:33 +0000

    Use Python to build from markdown

---
+++ compile.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+import markdown as md
+from copy import deepcopy
+import shutil
+import os
+
+
+def normalize(self, target):
+    """
+    Inverse of relative_to, honestly no idea why this isn't already in pathlib.Path
+    """
+    rel = target.relative_to(self)
+    norm = Path()
+    for p in rel.parents:
+        if p != Path():
+            norm /= ".."
+    return norm
+Path.normalize = normalize
+
+
+# Define some useful paths
+root = Path(__file__).parent
+build = root / "build"
+source = root / "source"
+# Read in template
+template = (root / "template.html").read_text()
+
+
+def buildPage(file):
+    # Copy template
+    page = deepcopy(template)
+    # Transpile html content
+    content_md = file.read_text()
+    content_html = md.markdown(content_md)
+    page = page.replace("{{content}}", content_html)
+    # Mark asset paths as needing normalization
+    page = page.replace("src=\"_assets/", "src=\"{{assets}}/")
+    # Normalize paths
+    for key in ("style", "utils", "assets"):
+        norm = source.normalize(file) / ("_" + key)
+        page = page.replace("{{%s}}" % key, str(norm).replace("\\", "/"))
+    # Where to write html file to?
+    outpath = build / file.relative_to(source).parent / (file.stem + ".html")
+    # Make sure directory exists
+    if not outpath.parent.is_dir():
+        os.makedirs(str(outpath.parent))
+    # Write html file
+    outpath.write_text(page)
+
+
+# Copy style, assets and scripts over
+for key in ("_assets", "_style", "_utils"):
+    # Delete old build folder
+    if (build / key).is_dir():
+        shutil.rmtree(build / key)
+    # Copy source folder if there is one
+    if (source / key).is_dir():
+        shutil.copytree(
+            source / key,
+            build / key
+        )
+# Build every md file in source tree
+for file in source.glob("**/*.md"):
+    buildPage(file)
