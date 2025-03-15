23365f7d33c237bfede11bc462ba8849a5550e6b
TEParsons <todd.e.parsons@gmail.com>
2025-03-15 20:26:48 +0000

    SYS: Recognise relative html links as well as relative md links

--- utils/links.py
+++ utils/links.py
@@ -8,25 +8,40 @@ site_url = "/"
 source_dir = "/"
 
 
-def substitute_root_link(to_root: str, match: re.Match) -> str:
+def substitute_root_link(to_root: str, match: re.Match, output: str="md") -> str:
     """
     Make a root link (beginning with a slash) relative to the *site* root rather than the **server* root.
 
     Parameters
     ----------
+    to_root : str
+        Root path to make link relative to
     match : re.Match
         Regex match for the link
+    output : str
+        Either "md" for a markdown link or "html" for an html link, default is "md"
     """
     # get all groups
-    label, link = match.groups()
+    label = match.group("label")
+    link = match.group("link")
     # remove start if link includes root folder
     for pre in ("source/", ):
         if link.startswith(pre):
             link = link[len(pre):]
     # construct full link
-    full_link = f"[{label}]({to_root}/{link})"
+    if output == "md":
+        full_link = f"[{label}]({to_root}/{link})"
+    elif output == "html":
+        full_link = f"<a href={to_root}/{link}>{label}</a>"
+    else:
+        raise ValueError("Unrecognised output type '{output}', allowed values are 'md' or 'html'.")
 
     return full_link
+
+
+# create prepopulated susbtitution functions for md and html
+substitute_root_link_md = partial(substitute_root_link, output="md")
+substitute_root_link_html = partial(substitute_root_link, output="html")
 
 
 def on_page_markdown(markdown: str, page: pages.Page, config: config.Config, files: files.File) -> str:
@@ -54,13 +69,23 @@ def on_page_markdown(markdown: str, page: pages.Page, config: config.Config, fil
     if page.file.name != "index":
         num_back += 1
     to_root = "/".join([".."] * num_back)
-    # pre-populate substitution function with relative root string
-    repl = partial(substitute_root_link, to_root)
+    # pre-populate substitution functions with relative root string
+    repl_md = partial(substitute_root_link_md, to_root)
+    repl_html = partial(substitute_root_link_html, to_root)
     # regex to find root links
-    re_link = r"\[([^\]]*)\]\(\/([^\)]*)\)"
-    # do substitution
-    return re.sub(
-        pattern=re_link,
+    re_link_md = r"\[(?P<label>[^\]]*)\]\(\/(?P<link>[^\)]*)\)"
+    re_link_html = r"<a .*href=\"\/(?P<link>[^\">]*)\">(?P<label>[^<]*)</a>"
+    # do substitution in md
+    markdown = re.sub(
+        pattern=re_link_md,
         string=markdown, 
-        repl=repl
+        repl=repl_md
     )
+    # do substitution in html
+    markdown = re.sub(
+        pattern=re_link_html,
+        string=markdown, 
+        repl=repl_html
+    )
+
+    return markdown
